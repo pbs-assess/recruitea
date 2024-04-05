9411b183a5127fdfe3d0ffcde09ea32ffbc7f23e
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-04-05 09:57:16 -0700

    Add R/.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+##' Hake recruitment MCMC estimates as another example
+##'
+##' @format A tibble with each row representing an MCMC sample, and columns:
+##' \describe{
+##'   \item{Virgin:}{presumably gives `R0`, TODO double check}
+##'   \item{`1966`:`2027`:}{year for each recruitment}
+##'  }
+##' @examples
+##' @author Andrew Edwards
+##' @source Generated from running `data-raw/hake.R`
+"hake_recruitment_mcmc"
