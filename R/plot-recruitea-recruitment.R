c78d8b13a46af0065fea24d8592aed163caf7bf0
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-04-08 17:44:16 -0700

    Add plot.recruitea_recruitment() and document().

---
+++ R/plot-recruitea-recruitment.R
@@ -0,0 +1,118 @@
+##' Plot a recruitea recruitment time series object (currently assumes annual values)
+##'
+##' Temporal plot for a `recruitea` recruitment time series (of class
+##' `recruitea_recruitment`) object. Default is to show multiple MCMC samples.
+##' TODO Add an option to create pacea type recruitment plot.
+##'
+##' @param obj a `recruitea_recruitment` object, which is a tibble with rows
+##'   representing MCMC samples, the `unfished` column being the unfished
+##'   equilibrium, and remaining columns whose names are the years.
+##' @param xlab label for the x axis
+##' @param ylab label for the y axis, defaults to a `axis-name` attribute of `obj`
+##' @param subsample number of MCMC samples to plot
+##' @param unfished_years_before years before the start to plot the unfished
+##'   equilibrium recruitment
+##' @param y_tick_by increments for y tick marks
+##' @param start_decade_ticks decade to start decadal tickmarks
+##' @param y_max maximum y value for certain types of plot (use this if you get
+##'   an error when specifying `ylim`)
+##' @param ... further options passed onto `plot.default()`
+##' @return plot of the time series with `subsample` number of MCMC samples to
+##'   the current device; returns nothing.
+##' @export
+##' @author Andrew Edwards
+##' @examples
+##' \dontrun{
+##' plot(hake_recruitment_mcmc)
+##' plot(hake_recruitment_mcmc,
+##'      xlim = c(lubridate::dmy(01011950),
+##'               lubridate::dmy(01012040))) # to expand x-axis
+##' }
+plot.recruitea_recruitment <- function(obj,
+                                       xlab = "Year",
+                                       ylab = attr(obj, "axis_name"),
+                                       subsample = 20,
+                                       unfished_years_before = 3,
+                                       y_tick_by = 1,
+                                       x_tick_extra_years = 20,
+                                       start_decade_ticks = lubridate::ymd("1800-01-01",                                                                        truncated = 2),
+                                       y_max = NULL,
+                                       ...
+                                       ){
+
+  ## stopifnot("value must be a column of the recreatea_recruitment object" =
+  ##           value %in% names(obj))
+
+  ## stopifnot("function currently assumes annual (not monthly) recruitments" =
+  ##           !("month" %in% names(obj)))
+
+  # Doesn't really working making the column headings of the obj tibble be Dates
+  years_as_dates <- names(obj)
+  years_as_dates[1] <- as.character(as.numeric(years_as_dates[2]) - unfished_years_before)
+  years_as_dates <- lubridate::ymd(years_as_dates,
+                                   truncated = 2)
+
+  sample_rows <- sample(nrow(obj),
+                        subsample)
+
+  matplot(years_as_dates,
+          t(obj[sample_rows, ]),
+          type="l",
+          lty=1,
+          xlab = xlab,
+          ylab = ylab)
+
+  pacea::add_tickmarks(tibble(date = years_as_dates),
+                y_tick_by = y_tick_by,
+                y_tick_start = floor(par("usr")[3]),
+                y_tick_end = ceiling(par("usr")[4]),
+                x_tick_extra_years = x_tick_extra_years,
+                start_decade_ticks = start_decade_ticks)
+
+
+  ## if(grepl("over_2010", deparse(substitute(obj)))){
+  ##   add_line_at_1 = TRUE
+  ##   # Set these defaults if user hasn't changed from default
+  ##   if(uncertainty_bar_col == "blue"){
+  ##     uncertainty_bar_col = "red"
+  ##   }
+  ##   if(is.null(y_max)){
+  ##     y_max = 1.2
+  ##   }
+  ##   if(y_tick_by == 1){
+  ##     y_tick_by = 0.1
+  ##   }
+  ## }
+
+  ## if(grepl("over_R0", deparse(substitute(obj)))){
+  ##   add_line_at_1 = TRUE
+  ##   # Set these defaults if user hasn't changed from default
+  ##   if(uncertainty_bar_col == "blue"){
+  ##     uncertainty_bar_col = "red"
+  ##   }
+  ##   if(is.null(y_max)){
+  ##     y_max = 10
+  ##   }
+  ##   # y_tick_by 1 = default is fine
+  ## }
+
+  ## if(style == "uncertainty"){
+  ##   plot_with_uncertainty_discrete(obj_lub,
+  ##                                  value = value,
+  ##                                  xlab = xlab,
+  ##                                  ylab = ylab,
+  ##                                  uncertainty_bar_col = uncertainty_bar_col,
+  ##                                  y_max = y_max,
+  ##                                  add_line_at_1 = add_line_at_1,
+  ##                                  add_line_at_1_col = add_line_at_1_col,
+  ##                                  add_line_at_1_lty = add_line_at_1_lty,
+  ##                                  ...)
+  ## } else {
+  ##   plot.default(obj_lub$date,
+  ##                obj_lub[[value]], # [[]] returns a vector not a tibble
+  ##                xlab = xlab,
+  ##                ylab = ylab,
+  ##                ...)
+  ## }
+ invisible()
+}
